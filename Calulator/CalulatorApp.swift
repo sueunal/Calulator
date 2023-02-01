f53114f744d3084015b67ef158f6849c54f34c06
sueunal <doms@kakao.com>
2023-02-01 19:44:13 +0900

    Initial Commit

---
+++ Calulator/CalulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalulatorApp.swift
+//  Calulator
+//
+//  Created by sueun kim on 2023/02/01.
+//
+
+import SwiftUI
+
+@main
+struct CalulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
