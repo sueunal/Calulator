9e2ed2d11af504fff2ba38c8ec60b6e994cb66e3
sueunal <doms@kakao.com>
2023-02-05 17:10:32 +0900

    foreground color add

--- Calulator/ContentView.swift
+++ Calulator/ContentView.swift
@@ -9,29 +9,96 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var totalNumber : String = "0"
-    let buttonNumber : [[String]] = [
-        ["C","/","%","$"],
-        ["7", "8","9","X"],
-        ["4", "5","6","-"],
-        ["1", "2","3","+"],
-        ["0", "0",".","="]
+    
+    enum ButtonType:String {
+        case first, second, third, forth, fifth, sixth, seventh,
+             eighth, nineth, zero
+        case dot, equal, plus, minus, multiple, devide
+        case percent, opposite, clear
+        var ButtonDisplayName : String {
+            switch self {
+            case .first :
+                return "1"
+            case .second :
+                return "2"
+            case .third:
+                return "3"
+            case .forth:
+                return "4"
+            case .fifth:
+                return "5"
+            case .sixth:
+                return "6"
+            case .seventh:
+                return "7"
+            case .eighth:
+                return "8"
+            case .nineth:
+                return "9"
+            case .zero:
+                return "0"
+            case .dot:
+                return "."
+            case .equal:
+                return "="
+            case .plus:
+                return "+"
+            case .minus:
+                return "-"
+            case .multiple:
+                return "X"
+            case .devide:
+                return "$"
+            case .percent:
+                return "%"
+            case .opposite:
+                return "/"
+            case .clear:
+                return "C"
+            }
+        }
+        var backgroundColor : Color {
+            switch self {
+            case .first,.second,  .third, .fifth, .forth, .sixth, .seventh , .eighth, .nineth, .zero, .dot:
+                return Color("NumberButton")
+            case .equal, .plus, .minus, .multiple, .devide:
+                return Color.orange
+            case .percent, .opposite, .clear:
+                return Color.gray
+            }
+        }
+        var forgroundColor : Color {
+            switch self {
+            case .first,.second,  .third, .fifth, .forth, .sixth, .seventh , .eighth, .nineth, .zero, .dot, .equal, .plus, .minus, .multiple, .devide:
+                return Color.white
+            case .percent, .opposite, .clear:
+                return Color.black
+            }
+        }
+    }
+    let buttonNumber : [[ButtonType]] = [
+        [.clear, .opposite, .percent, .devide],
+        [.seventh, .eighth, .nineth, .multiple],
+        [.forth, .fifth, .sixth, .minus],
+        [.first, .second, .third, .plus],
+        [.zero, .zero, .dot, .equal]
     ]
-    var body: some View {
-        ZStack{
-            Color.black.ignoresSafeArea()
-            VStack {
-                Spacer()
-                HStack{
+        var body: some View {
+            ZStack{
+                Color.black.ignoresSafeArea()
+                VStack {
                     Spacer()
-                    Text(totalNumber)
-                        .font(.system(size:78))
-                        .foregroundColor(.white)
-                        .padding()
-                }
-                ForEach(buttonNumber,id:\.self) {line in
                     HStack{
-                        ForEach(line, id:\.self){ item in
-                                Text(item)
+                        Spacer()
+                        Text(totalNumber)
+                            .font(.system(size:78))
+                            .foregroundColor(.white)
+                            .padding()
+                    }
+                    ForEach(buttonNumber,id:\.self) {line in
+                        HStack{
+                            ForEach(line, id:\.self){ item in
+                                Text(item.ButtonDisplayName)
                                 Button{
                                     if totalNumber == "0"{
                                         totalNumber = "7"
@@ -39,23 +106,24 @@ struct ContentView: View {
                                         totalNumber += "7"
                                     }
                                 }label: {
-                                    Text(item)
+                                    Text(item.ButtonDisplayName)
                                         .frame(width: 80, height: 80)
-                                        .background(Color(("NumberButton")))
+                                        .background(item.backgroundColor)
+                                        .foregroundColor(item.forgroundColor)
                                         .cornerRadius(40)
                                         .foregroundColor(.white).bold()
                                         .font(.system(size:33))
                                     
                                 }
                             }
+                        }
                     }
                 }
-           }
+            }
+        }
+        struct ContentView_Previews: PreviewProvider {
+            static var previews: some View {
+                ContentView()
+            }
         }
     }
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
-    }
-}
